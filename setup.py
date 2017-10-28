f0b656a2955760bb7e2f79612fae9b5ed59478bd
Ryan Riddle <rcri222@gmail.com>
2017-10-28 01:59:23 -0700

    add license and readme

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'rc-social-rules',
+  packages = ['rc-social-rules'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Prints the Recurse Center logo and social rules to your terminal',
+  author = 'Ryan Riddle',
+  author_email = 'rcri222@gmail.com',
+  url = 'https://github.com/RyanRiddle/rc-social-rules', # use the URL to the github repo
+  download_url = 'https://github.com/RyanRiddle/rc-social-rules/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
